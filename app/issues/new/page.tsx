dc29f62580db1b64b9f14ce7a184474fcda1f834
robberbadeia <robber.badeia@gmail.com>
2024-08-16 00:35:58 +0300

    Add aditional loading skeltons

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,16 @@
 "use client";
-import { Box, Button, Callout, Text, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from "react-hook-form";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { AiFillInfoCircle } from "react-icons/ai";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "../../components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
